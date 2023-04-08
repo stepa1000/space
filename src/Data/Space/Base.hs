f314c953e5acb9a77222ebc098a0e591e9234fdd
stepa <stepa100023@gmail.com>
2023-04-08 21:32:49 +0600

    Space Universe

--- src/Data/Space/Base.hs
+++ src/Data/Space/Base.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Data.Space.Base where
@@ -5,13 +6,15 @@ module Data.Space.Base where
 import Control.Comonad.Trans.Adjoint as W
 import Control.Comonad.Trans.Class
 import Control.Core.Composition
+import Control.Monad.Co
+import Control.Monad.Trans.Adjoint as M
 import Data.Functor.Adjunction
 import Data.Universe
 
-type Space f g a = W.AdjointT f g Universe2 a
+type SpaceGlobal f g = W.AdjointT f g Universe2
 
-makeSpace :: (Monad m, Functor f) => f () -> g b -> g b -> a -> (a -> m (Maybe (g b, a))) -> m (Space f g b)
-makeSpace fa fgNil gCentr a mg = do
+makeSpaceG :: (Monad m, Functor f) => f () -> g b -> g b -> a -> (a -> m (Maybe (g b, a))) -> m (SpaceGlobal f g b)
+makeSpaceG fa fgNil gCentr a mg = do
   ll <- fixF a
   rl <- fixF a
   ll' <- fixF a
@@ -32,17 +35,37 @@ makeSpace fa fgNil gCentr a mg = do
           return $ gb : gbl
         Nothing -> return $ repeat fgNil
 
-upS :: (Adjunction f g) => Space f g a -> Space f g a
-upS = hoistWAdj up
+{-
+makeSpaceDS :: (Functor f) => f () -> g b -> a -> (a -> Maybe (g b, a)) -> SpaceGlobal f g b
+makeSpaceDS fu a gd gmd =
+  fst <$> Universe2 $
+    makeUniverse
+      (const $ makeUniverse f f (gmd a))
+      (const $ makeUniverse f f (gmd a))
+      (makeUniverse f f (gmd a))
+  where
+    f (Just gmba) = gmb (snd gmba)
+    f _ = Nothing
+-}
+upSG :: (Adjunction f g) => SpaceGlobal f g a -> SpaceGlobal f g a
+upSG = hoistWAdj up
 
-downS :: (Adjunction f g) => Space f g a -> Space f g a
-downS = hoistWAdj down
+downSG :: (Adjunction f g) => SpaceGlobal f g a -> SpaceGlobal f g a
+downSG = hoistWAdj down
 
-leftS :: (Adjunction f g) => Space f g a -> Space f g a
-leftS = hoistWAdj left2
+leftSG :: (Adjunction f g) => SpaceGlobal f g a -> SpaceGlobal f g a
+leftSG = hoistWAdj left2
 
-rightS :: (Adjunction f g) => Space f g a -> Space f g a
-rightS = hoistWAdj right2
+rightSG :: (Adjunction f g) => SpaceGlobal f g a -> SpaceGlobal f g a
+rightSG = hoistWAdj right2
 
-takeRange2S :: (Adjunction f g) => (Int, Int) -> (Int, Int) -> Space f g a -> [[a]]
-takeRange2S px py u = takeRange2 px py $ lower u
+takeRange2SG :: (Adjunction f g) => (Int, Int) -> (Int, Int) -> SpaceGlobal f g a -> [[a]]
+takeRange2SG px py u = takeRange2 px py $ lower u
+
+type Space fl gl fg gg m = M.AdjointT fl gl (CoT (SpaceGlobal fg gg) m)
+
+hoistWCoT :: (forall r. w2 (b -> m r) -> w1 (a -> m r)) -> CoT w1 m a -> CoT w2 m b
+hoistWCoT f (CoT fw) = CoT $ fw . f
+
+-- makeSpace :: (Monad m, Functor f) => f () -> a -> (a -> (Maybe (g (m (fl b)), a))) -> Space fl gl f g m b
+-- makeSpace fa fgNil gCentr a mg = do
